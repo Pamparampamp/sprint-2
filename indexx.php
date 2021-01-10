0d69ae7996673bf04e2df44bd728b27450eec383
arnas112 <72792606+arnas112@users.noreply.github.com>
2021-01-10 12:44:41 +0200

    2

--- indexx.php
+++ indexx.php
@@ -6,7 +6,7 @@ if (!$conn) {
 die('Connection failed: ' . mysqli_connect_error());
 }
 if(isset($_GET['action']) and $_GET['action'] == 'delete'){
-$sql = 'DELETE FROM Employees WHERE id = ?';
+$sql = 'DELETE FROM projectss WHERE id = ?';
 $stmt = $conn->prepare($sql);
 $stmt->bind_param('i', $_GET['id']);
 $res = $stmt->execute();
@@ -21,7 +21,7 @@ die();
 <head>
 <meta charset='UTF-8'>
 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-<title>DARBUOTOJAI</title>
+<title>PROJEKTAI</title>
 <style>
 * {
 font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif;
@@ -51,13 +51,13 @@ color: white;
 </head>
 <body>
 <?php
-$sql = 'SELECT id, firstname, lastname FROM Employees';
+$sql = 'SELECT id, firstname, lastname, project FROM projectss';
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
-    print('<a href="indexx.php"><button>PROJEKTAI</button></a>');
+  print('<a href="index.php"><button>DARBUOTOJAI</button></a>');
 print('<table>');
 print('<thead>');
-print('<tr><th>Id</th><th>Name</th><th>Surname</th><th>Actions</th></tr>');
+print('<tr><th>Id</th><th>Name</th><th>Surname</th><th>Projects</th><th>Actions</th></tr>');
 print('</thead>');
 print('<tbody>');
 while($row = mysqli_fetch_assoc($result)) {
@@ -65,6 +65,7 @@ print('<tr>'
 . '<td>' . $row['id'] . '</td>'
 . '<td>' . $row['firstname'] . '</td>'
 . '<td>' . $row['lastname'] . '</td>'
+. '<td>' . $row['project'] . '</td>'
 . '<td>' . '<a href="?action=delete&id=' . $row['id'] . '"><button>DELETE</button></a>' . '</td>'
 . '</tr>');
 }
